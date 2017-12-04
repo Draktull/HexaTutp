064a5833b8ae6c66faaa6120dad382653f8c0e98
Theo <theo.petit.chaton@gmail.com>
2017-12-04 02:08:35 +0100

    day 1

---
+++ HexaTuto/Assets/HexGrid.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HexGrid : MonoBehaviour
+{
+    public int witdh = 6;
+    public int height = 6;
+
+    public HexCell cellprefab;
+    public Text cellLabelPrefab; 
+
+    public HexCell[] cells;
+
+    private HexMesh _mesh;
+    private Canvas _gridCanvas;
+
+    void Awake()
+    {
+        _mesh = GetComponentInChildren<HexMesh>();
+        _gridCanvas = GetComponentInChildren<Canvas>();
+        cells = new HexCell[witdh * height];
+
+        for (int z = 0, i = 0; z < height; z++)
+            for (int x = 0; x < witdh; x++)
+                CreateCell(cellprefab, x, z, i++);
+
+    }
+
+    void Start() {
+        _mesh.Triangulate(cells);
+    }
+
+    void CreateCell(HexCell cellPrefab,int x,int z,int i)
+    {
+        Vector3 position;
+        position.x = (x+(0.5f*(z%2f))) * HexMetrics.innerRadius*2;
+        position.y = 0f;
+        position.z = z* HexMetrics.outerRadius*1.5f;
+        HexCell cell = cells[i] = Instantiate<HexCell>(cellprefab);
+        cell.transform.SetParent(this.transform, false);
+        cell.transform.localPosition = position;
+        cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+        //label
+        Text label = Instantiate<Text>(cellLabelPrefab);
+        label.rectTransform.SetParent(_gridCanvas.transform, false);
+        label.rectTransform.anchoredPosition = new Vector2(position.x, position.z);
+        label.text = cell.coordinates.ToStringOnSeparateLines();
+
+    }
+
+}
---
+++ HexaTuto/Assets/HexMetrics.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class HexMetrics  {
+
+	public const float outerRadius  = 10f;
+	public const float innerRadius = outerRadius *  0.866025404f; // phytagore
+
+	public static Vector3[] corners = {
+		new Vector3 (0, 0, outerRadius),
+		new Vector3(innerRadius,0, outerRadius*0.5f),
+		new Vector3(innerRadius,0, outerRadius*-0.5f),
+		new Vector3 (0, 0, -outerRadius),
+		new Vector3 (-innerRadius, 0, outerRadius*-0.5f),
+		new Vector3 (-innerRadius, 0, outerRadius*0.5f),
+        new Vector3 (0, 0, outerRadius),
+    };
+		
+}
